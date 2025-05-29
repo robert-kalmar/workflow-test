2ddff647f5d6a29b8d449b95781c9aca406129b4
Robert Kalmar <robert.kalmar@nxp.com>
2025-05-29 10:37:34 +0200

    small update

--- .github/workflows/run_pip.sh
+++ .github/workflows/run_pip.sh
@@ -2,7 +2,8 @@ set +vx
 
 pwd
 echo `which python` 
-for i in {1..100}; do 
+python --version
+for i in {1..10}; do
 	echo "Attempt $i"; 
 	pip install --extra-index-url https://eiq.nxp.com/repository neutron-converter_SDK_25_03 --no-cache-dir;
 	pip uninstall neutron_converter_SDK_25.03 --yes ;
