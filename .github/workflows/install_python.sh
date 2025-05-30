76d05aa0007141aadf5e3949d1b5a6aaf2f7afc8
Robert Kalmar <robert.kalmar@nxp.com>
2025-05-30 08:18:26 +0200

    add workflow dispatch event

--- .github/workflows/install_python.sh
+++ .github/workflows/install_python.sh
@@ -4,26 +4,11 @@ echo "CI: Installing Python 3.10..."
 pwd
 uname -a
 sudo apt update
-# sudo apt install -y build-essential libncursesw5-dev libreadline-gplv2-dev libssl-dev libgdbm-dev libc6-dev libsqlite3-dev libbz2-dev libffi-dev liblzma-dev
 sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev liblzma-dev tk-dev
 
 curl https://pyenv.run | bash
 $HOME/.pyenv/bin/pyenv install 3.10 3.11 3.12
-
 $HOME/.pyenv/bin/pyenv global 3.10 3.11 3.12
-
-# echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
-# echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
-# echo 'eval "$(pyenv init - bash)""' >> ~/.profile
-#
-# echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
-# echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
-# echo 'eval "$(pyenv init - bash)""' >> ~/.bash_profile
-#
-#
-# echo 'export PYENV_ROOT="$HOME/.pyenv"' >>  ~/.bashrc
-# echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >>  ~/.bashrc
-# echo 'eval "$(pyenv init - bash)""' >>  ~/.bashrc
 
 echo "CI: Pythons succesfully installed."
 
--- .github/workflows/run_pip.sh
+++ .github/workflows/run_pip.sh
@@ -3,27 +3,7 @@ set +vx
 pwd
 
 eval "$($HOME/.pyenv/bin/pyenv init - bash)"
-# echo "Shell is:  $SHELL"
-# #$HOME/.pyenv/bin/pyenv shell 3.{$1}
-# $HOME/.pyenv/bin/pyenv global 3.10 #3.11 3.12
-# echo "Versions: "
-# $HOME/.pyenv/bin/pyenv versions
-# echo "prefixes"
-# $HOME/.pyenv/bin/pyenv prefix
-# echo "global"
-# $HOME/.pyenv/bin/pyenv global
-# echo "shims"
-# $HOME/.pyenv/bin/pyenv shims
-# echo `which python3.$1`
-# echo `which python`
-# python --version
-# pip --version
-# python3.$1 --version
 
-# echo "let just blindly run pip"
-# $HOME/.pyenv/shims/pip3.$1 --version
-# $HOME/.pyenv/shims/pip3.$1 install --extra-index-url https://eiq.nxp.com/repository neutron_converter_SDK_25_03 --no-cache-dir;
-# $HOME/.pyenv/shims/pip3.$1 uninstall neutron-converter_SDK_25.03 --yes ;
 for i in {1..10}; do
 	echo "Attempt $i"; 
 	pip3.$1 install --extra-index-url https://eiq.nxp.com/repository neutron_converter_SDK_25_03 --no-cache-dir;
